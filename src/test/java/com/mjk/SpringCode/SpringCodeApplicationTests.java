c23fb408fd418587c379b6e60a30de74afdfd322
Michael Killeen <mkilleen@meetveracity.com>
2020-08-12 10:06:45 -0400

    Initial

---
+++ src/test/java/com/mjk/SpringCode/SpringCodeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjk.SpringCode;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCodeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
